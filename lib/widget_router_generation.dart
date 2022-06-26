4cd4ee379328c4339e0b02a40531e55fe0a3e775
Julian Schumacher <Jules.media@outlook.de>
2022-06-26 11:47:12 +0200

    Update Import

--- lib/widget_router_generation.dart
+++ lib/widget_router_generation.dart
@@ -2,7 +2,7 @@ library widget_router;
 
 import 'package:build/build.dart' show Builder, BuilderOptions;
 import 'package:source_gen/source_gen.dart' show SharedPartBuilder;
-import 'package:widget_router/src/build/routes_generator.dart'
+import 'package:widget_router/src/builder/routes_generator.dart'
     show RoutesGenerator;
 
 /// the Builder called for the Widget Router Package
